ea72ae00a716b4917a11520b6d3d4c2b91810c1a
Brian Mock <mock.brian@gmail.com>
2012-01-04 01:49:40 -0800

    Xfwm4 themannnnnnnnn...

--- themes/MTest/xfwm4/title-linker.sh
+++ themes/MTest/xfwm4/title-linker.sh
@@ -1,10 +1,9 @@
-#for i in 2 3 4 5; do
-#    ln -s "title-1-active.xpm" "title-$i-active.xpm"
-#    ln -s "title-1-active.xpm" "title-$i-inactive.xpm"
-#done
-
-ln -s "title-1-active.png" "title-1-inactive.png"
+rm "title-1-inactive.xpm"
+ln -s "title-1-active.xpm" "title-1-inactive.xpm"
 for i in 2 3 4 5; do
-    ln -s "title-1-active.png" "title-$i-active.png"
-    ln -s "title-1-active.png" "title-$i-inactive.png"
+    rm "title-$i-active.xpm"
+    rm "title-$i-inactive.xpm"
+
+    ln -s "title-1-active.xpm" "title-$i-active.xpm"
+    ln -s "title-1-active.xpm" "title-$i-inactive.xpm"
 done
