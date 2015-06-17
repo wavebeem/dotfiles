be25f33e4e238548592002a68c9361bf0e398043
Brian Mock <mock.brian@gmail.com>
2015-06-17 10:21:17 -0700

    Removes useless -l

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,7 +1,7 @@
 function fish_prompt
-    set -l soon \U0001f51c
-    set -l hamburger \U0001f354
-    set -l poo \U0001f4a9
+    set soon \U0001f51c
+    set hamburger \U0001f354
+    set poo \U0001f4a9
     set_color normal
     echo
     echo -n "$poo  "
