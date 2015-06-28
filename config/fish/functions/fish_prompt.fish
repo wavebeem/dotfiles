2f8773036ba3518e48f36380440cf461ac3347a8
Brian Mock <mock.brian@gmail.com>
2015-06-27 18:16:04 -0700

    Change prompt over time

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,8 +1,37 @@
 function fish_prompt
-    set soon \U0001f51c
-    set hamburger \U0001f354
-    set poo \U0001f4a9
+    set soon \U1f51c
+    set hamburger \U1f354
+    set poo \U1f4a9
+    set tengu \U1f47a
+    set watermelon \U1f349
+    set beginner \U1f530
+    set cyclone \U1f300
+
+    switch (date +%H)
+    case 0 1 2 3 4 5 6 7
+        set symbol $watermelon
+    case 8 9 10
+        set symbol $tengu
+    case 11 12
+        set symbol $hamburger
+    case '*'
+        set symbol $poo
+    end
+
+    # set choices \
+    #     $soon \
+    #     $hamburger \
+    #     $poo \
+    #     $tengu \
+    #     $watermelon \
+    #     $beginner \
+    #     $cyclone
+    # set n (math (random)%(count $choices))
+    # set m (math $n + 1)
+    # set symbol $choices[$m]
+
     set_color normal
     echo
-    echo -n "$poo  "
+    echo -n $symbol
+    echo -n "  "
 end
