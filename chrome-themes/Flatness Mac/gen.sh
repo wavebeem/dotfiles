3d9c4d8e207076fe6e9edc56d6adb7f960baa9c9
Brian Mock <brian@mockbrian.com>
2015-11-23 16:58:55 -0800

    New Chrome theme

---
+++ chrome-themes/Flatness Mac/gen.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -e
+
+dir="img"
+
+if which gm >/dev/null; then
+    _ImgConvert='gm convert'
+elif which convert >/dev/null; then
+    _ImgConvert='convert'
+else
+    echo "Please install either ImageMagick or GraphicsMagick" 1>&2
+    exit 1
+fi
+
+ImgConvert() {
+    $_ImgConvert "$@"
+}
+
+Build() {
+    local color="$1"
+    local name="$2"
+    ImgConvert "xc:#$color" -size 20x20 "$dir/$name.png"
+}
+
+Build dddddd frame
+Build ffffff frame_inactive
+Build dddddd tab_background
+Build eeeeee toolbar
