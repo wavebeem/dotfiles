bac32080c50fa27c2a3054d672368b257feba132
Brian Mock <brian@mockbrian.com>
2016-04-30 09:23:46 -0700

    sublime

--- themes/Eminence-X3-2/xfwm4/border.sh
+++
@@ -1,1 +0,0 @@
-find -type f -name '*.png' | egrep -v 'close|screenshot' | xargs gimp
--- themes/Eminence-X3-5/xfwm4/linker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for active in *-active.*; do
-    inactive=${active/active/inactive}
-
-    rm "$inactive"
-    ln -s "$active" "$inactive"
-done
