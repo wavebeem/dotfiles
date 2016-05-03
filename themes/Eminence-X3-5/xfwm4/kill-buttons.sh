5d7a6ba79386a35e5a7babc2acc1a7bd6fe3bdb8
Brian Mock <brian@mockbrian.com>
2016-05-02 17:21:29 -0700

    sublime

--- themes/Eminence-X3-5/xfwm4/kill-buttons.sh
+++
@@ -1,4 +0,0 @@
-for icon in close-*.xpm maximize-*.xpm shade-*.xpm stick-*.xpm hide-*.xpm; do
-    rm "$icon"
-    ln -s "empty.xpm" "$icon"
-done
--- themes/Eminence-X4/xfwm4/deco.sh
+++
@@ -1,1 +0,0 @@
-gimp bottom-active.png title-1-active.png top-left-active.png top-right-active.png left-active.png right-active.png
