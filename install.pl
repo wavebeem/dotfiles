e9799e81b530e029bf74e9b52faacb0380fb2095
Brian Mock <brian@mockbrian.com>
2016-06-15 17:17:20 -0700

    Un-nest that sublime garbage finally

--- install.pl
+++ install.pl
@@ -4,42 +4,57 @@ use warnings;
 use File::Path qw(make_path remove_tree);
 use File::Basename;
 
+my $HOME = $ENV{HOME};
+
 sub install {
-    my ($file) = @_;
-    my $dotless = $file;
-    $dotless =~ s/^\.//;
-    my $HOME = $ENV{HOME};
-    my $src = "$HOME/dotfiles/$dotless";
-    my $dst = "$HOME/$file";
-    my $dir = dirname($dst);
-    if (-l $dst) {
-        unlink($dst);
-    }
-    elsif (-e $dst) {
-        print("Delete $dst? [y/N] ");
-        return unless <STDIN> =~ /^y/i;
-        remove_tree($dst);
-    }
-    print("$src -> $dst\n");
-    make_path($dir) unless -d $dir;
-    symlink($src, $dst);
+  my ($file) = @_;
+  my $dst = "$HOME/$file";
+  install_as($file, $dst);
 }
 
-install($_) for (
-    ".hushlogin",
-    ".jscsrc",
-    ".welcome",
-    ".ackrc",
-    ".dircolors",
-    ".zshenv",
-    ".vimrc",
-    ".zshrc",
-    ".bashrc",
-    ".bash_profile",
-    ".tmux.conf",
-    ".inputrc",
-    ".gitconfig",
-    ".config/fish/config.fish",
-    ".config/fish/functions",
-    "Library/Application Support/Sublime Text 3/Packages/User",
-);
+sub install_as {
+  my ($file, $dst) = @_;
+  my $dotless = $file;
+  $dotless =~ s/^\.//;
+  my $src = "$HOME/dotfiles/$dotless";
+  my $dir = dirname($dst);
+  if (-l $dst) {
+    unlink($dst);
+  }
+  elsif (-e $dst) {
+    print("Delete $dst? [y/N] ");
+    return unless <STDIN> =~ /^y/i;
+    remove_tree($dst);
+  }
+  print("$src -> $dst\n");
+  make_path($dir) unless -d $dir;
+  symlink($src, $dst);
+}
+
+my $normals = [
+  ".hushlogin",
+  ".jscsrc",
+  ".welcome",
+  ".ackrc",
+  ".dircolors",
+  ".zshenv",
+  ".vimrc",
+  ".zshrc",
+  ".bashrc",
+  ".bash_profile",
+  ".tmux.conf",
+  ".inputrc",
+  ".gitconfig",
+  ".config/fish/config.fish",
+  ".config/fish/functions",
+];
+
+my $specials = [
+  [
+    "_sublime",
+    "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+  ]
+];
+
+install($_) for @$normals;
+install_as(@$_) for @$specials;
