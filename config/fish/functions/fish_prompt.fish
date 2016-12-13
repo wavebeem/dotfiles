2794ce4dc6842f8aa3b365ab46b04a6f8d00d7c7
Brian Mock <brian@mockbrian.com>
2016-12-12 20:17:47 -0800

    Simplify prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,30 +1,15 @@
 function fish_prompt
-  set E
-  set E $E \U1F51C # soon
-  set E $E \U1F354 # hamburger
-  set E $E \U1F4A9 # poo
-  set E $E \U1F47A # tengu
-  set E $E \U1F349 # watermelon
-  set E $E \U1F300 # cyclone
-  set E $E \U1F37A # beer
-  set E $E \U1F382 # cake
-  set E $E \U1F369 # donut
-  set E $E \U1F344 # mushroom
-  set E $E \U2615  # coffee
-  set E $E \U1F35C # ramen
-  set E $E \U1F363 # sushi
-  set E $E \U1F36A # cookie
-  set E $E \U1F36B # chocolate
-  set E $E \U1F371 # bento
-  set E $E \U1F411 # sheep
-  set E $E \U1F525 # fire
+  set poo \U1F4A9
+  set lollipop \U1F36D
+  set arrow "►"
 
-  set n (math (random)%(count $E))
-  set m (math $n+1)
-  set symbol $E[$m]
+  if test $status -eq 0
+    set emoji $lollipop
+  else
+    set emoji $poo
+  end
 
-  set_color normal
+  set_color reset
   echo
-  echo -n $symbol
-  echo "  "
+  echo -n "$emoji  $arrow "
 end
