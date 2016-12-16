2f35867aa86be4321c43842dd5a91a6cda226f66
Brian Mock <brian@mockbrian.com>
2016-12-15 16:19:06 -0800

    No more emoji prompts

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,14 +1,13 @@
 function fish_prompt
-  set poo \U1F4A9
-  set lollipop \U1F36D
-
-  if test $status -eq 0
-    set emoji $lollipop
-  else
-    set emoji $poo
-  end
-
+  set last_status $status
   set_color reset
+  if test $last_status -eq 0
+    set_color green
+    set char "●"
+  else
+    set_color red
+    set char "✖︎"
+  end
   echo
-  echo -n "$emoji  "
+  echo -n "$char "
 end
