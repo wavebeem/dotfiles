806f7a04f769c426238634e6efa2b578d1156dbf
Brian Mock <brian@mockbrian.com>
2017-02-03 09:37:31 -0800

    fishy prompty

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -2,14 +2,14 @@ function fish_prompt
   set last_status $status
   set_color reset
   if test $last_status -eq 0
-    set_color magenta
-    set char "✿"
+    set_color green
+    set char "=>"
   else
     set_color red
-    set char "✖︎"
+    set char "!!"
   end
   echo
   echo -n $char
   set_color reset
-  echo -n "  "
+  echo -n " "
 end
