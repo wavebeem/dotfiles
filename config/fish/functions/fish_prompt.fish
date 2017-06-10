d73a0b7847e047e8e6ccfe49204ef21acc8d89ec
Brian Mock <brian@mockbrian.com>
2017-06-09 17:12:17 -0700

    prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,15 +1,20 @@
 function fish_prompt
   set last_status $status
   set_color reset
-  if test $last_status -eq 0
-    set_color green
-    set char "=>>"
-  else
-    set_color red
-    set char "***"
-  end
   echo
-  echo -n $char
+  set light aaa
+  set_color $light --bold
+  echo -n "::: "
+  set_color cyan --bold
+  set user (whoami)
+  set host (hostname -s)
+  echo -n $user
+  set_color $light --bold
+  echo -n " ::: "
+  set_color magenta --bold
+  echo -n $host
+  set_color $light --bold
+  echo -n " :::"
   set_color reset
   echo -n " "
 end
