0e5d9338d742ad94b2315532afcb8c6f07969caa
Brian Mock <brian@mockbrian.com>
2017-07-03 17:42:26 -0700

    multiline prompt???

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,17 +1,30 @@
 function fish_prompt
   set_color reset
 
-  # Apparently you actually have to quote for `test` with fish :/
-  if test -n "$VIRTUAL_ENV"
-    set_color aaa --bold
-    echo -n "(venv) "
-  end
+  set home ~
+  set cwd (pwd | sed "s|$home|~|")
 
   set_color green --bold
   echo -n (hostname -s)
 
   set_color aaa --bold
-  echo -n " :::"
+  echo -n " :: "
+
+  set_color black --bold
+  echo -n $cwd
+
+  # Apparently you actually have to quote for `test` with fish :/
+  if test -n "$VIRTUAL_ENV"
+    set_color aaa --bold
+    echo -n " :: "
+    set_color aaa --bold
+    echo -n "venv"
+  end
+
+  echo
+
+  set_color aaa --bold
+  echo -n "=>>"
 
   set_color reset
   echo -n " "
