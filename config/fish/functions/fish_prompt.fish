791ad8ad71351c2d998a58c1843d5ebd09b906cc
Brian Mock <brian@mockbrian.com>
2017-07-26 09:26:02 -0700

    fish cleanup

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,27 +1,24 @@
 function fish_prompt
-  set c_reset (set_color normal)
-  set c_bold (set_color normal)(set_color --bold)
-  set c_light (set_color aaa --bold)
-  set c_green (set_color green --bold)
+  set cr (set_color normal)
+  set c1 $cr(set_color --bold)
+  set c2 $cr(set_color green --bold)
+  set c3 $cr
+  set separator " :: "
 
-  printf "%s" $c_reset
-
-  set separator "$c_reset :: "
-
-  printf "%s%s" $c_bold (whoami)
-  printf "%s" $separator
-  printf "%s%s" $c_green (hostname -s)
-  printf "%s" $separator
-  printf "%s%s" $c_bold (prompt_pwd)
+  printf "%s%s" $c1 (whoami)
+  printf "%s%s" $c3 $separator
+  printf "%s%s" $c2 (hostname -s)
+  printf "%s%s" $c3 $separator
+  printf "%s%s" $c1 (prompt_pwd)
 
   if not set -q $VIRTUAL_ENV
-    printf "%s" $separator
-    printf "%s%s" $c_bold "venv"
+    printf "%s%s" $c3 $separator
+    printf "%s%s" $c1 "venv"
   end
 
   echo
 
-  printf "%s%s%s " $c_bold ">>-" $c_reset
+  printf "%s%s%s " $c1 ">>-" $cr
 end
 
 # I would totally put this in a separate file, but because functions are
