beebd77664191df9a458d327f88e534989e8de43
Brian Mock <brian@mockbrian.com>
2018-12-13 10:02:29 -0800

    nice

--- BetterTouchTool/bar-info.js
+++ BetterTouchTool/bar-info.js
@@ -1,16 +1,35 @@
+#!/usr/bin/env node
 const { execSync } = require("child_process");
 
 const sep = " ";
-const str = execSync("pmset -g batt", { encoding: "utf-8" });
-const [, level] = str.match(/(\d+)%/);
-const pct = Number(process.argv[2] || level);
-const symbols = {
-  none: "\u{1f311}",
-  less: "\u{1f318}",
-  half: "\u{1f317}",
-  more: "\u{1f316}",
-  full: "\u{1f315}"
+const pct = (() => {
+  if (2 in process.argv) {
+    return Number(process.argv[2]);
+  }
+  const str = execSync("pmset -g batt", { encoding: "utf-8" });
+  const [, level] = str.match(/(\d+)%/) || [];
+  return Number(level);
+})();
+
+const allSymbols = {
+  moons: {
+    none: "\u{1f311}",
+    less: "\u{1f318}",
+    half: "\u{1f317}",
+    more: "\u{1f316}",
+    full: "\u{1f315}"
+  },
+  bars: {
+    none: "▁",''
+    less: "▄",
+    half: "▄",
+    more: "▄",
+    full: "█"
+  }
 };
+
+const symbols = allSymbols.bars;
+
 const before = Math.floor(pct / 10);
 const after = Math.floor((100 - pct) / 10);
 const out = [];
