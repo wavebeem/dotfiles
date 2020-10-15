536f9b6d9542f2711b67da0d812d7750deebf4a9
Brian Mock <brian@wavebeem.com>
2020-10-14 18:05:25 -0700

    simpler powershell

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,24 +1,24 @@
-$c1 = "Yellow"
+$color = "Yellow"
 
 Set-PSReadlineOption -BellStyle None
 Set-PSReadlineOption -EditMode Emacs
 
 Set-PSReadLineOption -Colors @{
-  ContinuationPrompt = $c1
-  Emphasis = $c1
-  Error = $c1
-  Selection = $c1
-  Default = $c1
-  Comment = $c1
-  Keyword = $c1
-  String = $c1
-  Operator = $c1
-  Variable = $c1
-  Command = $c1
-  Parameter = $c1
-  Type = $c1
-  Number = $c1
-  Member = $c1
+  ContinuationPrompt = $color
+  Emphasis = $color
+  Error = $color
+  Selection = $color
+  Default = $color
+  Comment = $color
+  Keyword = $color
+  String = $color
+  Operator = $color
+  Variable = $color
+  Command = $color
+  Parameter = $color
+  Type = $color
+  Number = $color
+  Member = $color
 }
 
 $host.PrivateData.ErrorForegroundColor = "Red"
@@ -27,49 +27,12 @@ $host.PrivateData.ErrorBackgroundColor = $host.ui.RawUI.BackgroundColor
 Set-Alias l ls
 Set-Alias ll ls
 
-function _wc($bg, $fg, $str) {
-  Write-Host -NoNewline -BackgroundColor $bg -ForegroundColor $fg $str
-}
-
 function prompt {
   Write-Host ""
-  Write-Host -NoNewline -ForegroundColor Cyan "$>"
+  Write-Host -NoNewline -ForegroundColor Cyan "pwsh:"
   return " "
-}
-
-function dim {
-  $size = $host.UI.RawUI.WindowSize
-  Write-Host $size.Width "by" $size.Height
-}
-
-function rmrf {
-  Remove-Item -Recurse -Force $args
 }
 
 function .. {
   Set-Location ..
-}
-
-function g {
-  git status
-}
-
-function gco {
-  git checkout $args
-}
-
-function gaa {
-  git add -A $args
-}
-
-function gg {
-  git commit $args
-}
-
-function gd {
-  git diff $args
-}
-
-function gdd {
-  git diff --cached $args
 }
