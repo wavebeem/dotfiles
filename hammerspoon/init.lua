4b5f3ff9604ca06fdde21798877a728fce67113b
Brian Mock <brian@mockbrian.com>
2020-12-09 10:49:14 -0800

    alfred

--- hammerspoon/init.lua
+++
@@ -1,9 +0,0 @@
-prefix = {"ctrl", "alt"}
-
-hs.hotkey.bind(prefix, "t", function()
-  hs.eventtap.keyStrokes(os.date("%Y-%m-%d %H.%M.%S"))
-end)
-
-hs.hotkey.bind(prefix, "d", function()
-  hs.eventtap.keyStrokes(os.date("%Y-%m-%d"))
-end)
