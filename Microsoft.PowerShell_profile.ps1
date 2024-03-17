7fc63ca34c49848b2277b02830da61e82355fbab
Sage Fennel <sage@wavebeem.com>
2024-03-16 18:29:58 -0700

    idk

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,33 +2,33 @@ Set-PSReadlineOption -BellStyle None
 Set-PSReadlineOption -EditMode Emacs
 
 Set-PSReadLineOption -Colors @{
-  Command = 'Yellow'
-  Number = 'Gray'
-  Member = 'Gray'
-  Operator = 'Gray'
-  Type = 'Gray'
-  Variable = 'Green'
-  Parameter = 'Green'
-  ContinuationPrompt = 'Gray'
-  Default = 'Gray'
+  Command = "Yellow"
+  Number = "Gray"
+  Member = "Gray"
+  Operator = "Gray"
+  Type = "Gray"
+  Variable = "Magenta"
+  Parameter = "Magenta"
+  ContinuationPrompt = "Gray"
+  Default = "Gray"
 }
 
-$Host.PrivateData.ErrorForegroundColor = "Red"
-$Host.PrivateData.ErrorBackgroundColor = "Black"
-$Host.PrivateData.WarningForegroundColor = "Yellow"
-$Host.PrivateData.WarningBackgroundColor = "Black"
-$Host.PrivateData.DebugForegroundColor = "Yellow"
-$Host.PrivateData.DebugBackgroundColor = "Black"
-$Host.PrivateData.VerboseForegroundColor = "Yellow"
-$Host.PrivateData.VerboseBackgroundColor = "Black"
-$Host.PrivateData.ProgressForegroundColor = "DarkGray"
-$Host.PrivateData.ProgressBackgroundColor = "Black"
+$x = $Host.PrivateData
+$x.ErrorForegroundColor = "Red"
+$x.ErrorBackgroundColor = "Black"
+$x.WarningForegroundColor = "Yellow"
+$x.WarningBackgroundColor = "Black"
+$x.DebugForegroundColor = "Yellow"
+$x.DebugBackgroundColor = "Black"
+$x.VerboseForegroundColor = "Yellow"
+$x.VerboseBackgroundColor = "Black"
+$x.ProgressForegroundColor = "DarkGray"
+$x.ProgressBackgroundColor = "Black"
 
 Set-Alias g "git status"
 
 $esc = [char]27
 $bold = "$esc[1m"
-
 
 function __install.eza {
   winget install eza-community.eza
@@ -47,7 +47,7 @@ if (Get-Command eza -ErrorAction SilentlyContinue) {
 
 function prompt {
   Write-Host ""
-  Write-Host -NoNewline -ForegroundColor Magenta "${bold}pwsh:"
+  Write-Host -NoNewline -ForegroundColor Green "${bold}pwsh:"
   return " "
 }
 
