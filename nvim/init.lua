b0a3e3cc969c63bc79a19ccb907f31091741446b
Sage Fennel <sage@wavebeem.com>
2026-07-22 11:29:34 -0700

    colors

--- nvim/init.lua
+++ nvim/init.lua
@@ -24,7 +24,7 @@ lazy.setup({
     lazy = false,
     priority = 1000,
     config = function()
-      vim.g.gruvbox_contrast_dark = "hard"
+      -- vim.g.gruvbox_contrast_dark = "hard"
       vim.cmd.colorscheme("gruvbox")
     end,
   },
