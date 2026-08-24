98296541221b7dbcc1ad34caacfeb02257ff98d6
Sage Fennel <sage@wavebeem.com>
2026-08-24 13:09:21 -0700

    more window styles

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,7 +1,56 @@
+prefix = {"ctrl", "alt"}
+
 -- Namespaces
 cmd = {}
-layout = {}
-g = {}
+
+function cmd.reload()
+  hs.reload()
+end
+
+hs.hotkey.bind(prefix, "r", cmd.reload)
+
+function main()
+    -- Configuration
+  hs.window.animationDuration = 0
+
+  hs.alert.defaultStyle.strokeWidth = 4
+  hs.alert.defaultStyle.strokeColor = { hex = "ffffff" }
+  hs.alert.defaultStyle.fillColor = { hex = "222222" }
+  hs.alert.defaultStyle.textColor = { hex = "ffffff" }
+  -- hs.alert.defaultStyle.textFont = ".AppleSystemUIFont"
+  hs.alert.defaultStyle.textSize = 24
+  hs.alert.defaultStyle.radius = 8
+  -- hs.alert.defaultStyle.atScreenEdge = 0
+  hs.alert.defaultStyle.fadeInDuration = 0
+  hs.alert.defaultStyle.fadeOutDuration = 0
+  hs.alert.defaultStyle.padding = 16
+
+  prefix = {"ctrl", "alt"}
+
+  hs.hotkey.bind(prefix, "y", cmd.unhideAll)
+  hs.hotkey.bind(prefix, "h", cmd.hidePersonalApps)
+
+  hs.hotkey.bind(prefix, "m", cmd.snapWindowLeftSmall)
+  hs.hotkey.bind(prefix, "u", cmd.snapWindowLeftBig)
+  hs.hotkey.bind(prefix, "j", cmd.snapWindowLeftHalf)
+  hs.hotkey.bind(prefix, "k", cmd.maximizeWindow)
+  hs.hotkey.bind(prefix, "i", cmd.snapWindowCenterBig)
+  hs.hotkey.bind(prefix, "l", cmd.snapWindowRightHalf)
+  hs.hotkey.bind(prefix, "o", cmd.snapWindowRightBig)
+  hs.hotkey.bind(prefix, ".", cmd.snapWindowRightSmall)
+  hs.hotkey.bind(prefix, ",", cmd.snapWindowCenterSmall)
+
+  hs.hotkey.bind(prefix, "d", cmd.typeDate)
+  hs.hotkey.bind(prefix, "t", cmd.typeTime)
+  hs.hotkey.bind(prefix, "x", cmd.typeUnixTime)
+
+  hs.hotkey.bind(prefix, "space", cmd.maximizeAllWindows)
+  hs.hotkey.bind(prefix, "v", cmd.showApplicationName)
+  hs.hotkey.bind(prefix, "r", cmd.reload)
+  hs.hotkey.bind(prefix, "c", cmd.toggleConsole)
+
+  hs.alert.show("Hammerspoon loaded", 0.5)
+end
 
 personalApps = {
   "Firefox Developer Edition",
@@ -21,11 +70,6 @@ function cmd.hidePersonalApps()
   end
 end
 
-function layout.new(name, size)
-  -- App, Window, Screen, Unit, Frame, Full
-  -- http://www.hammerspoon.org/docs/hs.layout.html#apply
-  return { name, nil, nil, size, nil, nil }
-end
 
 -- If this were a true "unhide all windows" it would uniminimize windows too.
 -- But that's slow, and I only ever use Hide Application rather than Minimize
@@ -44,39 +88,48 @@ function cmd.unhideAll()
   end
 end
 
-g.big = 5 / 8
-g.small = 1 - g.big
-g.left_big_rect = hs.geometry.rect(0, 0, g.big, 1)
-g.left_small_rect = hs.geometry.rect(0, 0, g.small, 1)
-g.right_small_rect = hs.geometry.rect(1 - g.small, 0, g.small, 1)
-g.right_big_rect = hs.geometry.rect(1 - g.big, 0, g.big, 1)
+do
+  local pad1 = 2/12
+  local pad2 = 4/12
+  local size1 = 4/12
+  local size2 = 8/12
+  local rect = hs.geometry.rect
 
-function cmd.snapWindowLeftBig()
-  hs.window.focusedWindow():moveToUnit(g.left_big_rect)
-end
+  function cmd.snapWindowCenterBig()
+    hs.window.focusedWindow():moveToUnit(rect(pad1, 0, size2, 1))
+  end
 
-function cmd.snapWindowLeftSmall()
-  hs.window.focusedWindow():moveToUnit(g.left_small_rect)
-end
+  function cmd.snapWindowCenterSmall()
+    hs.window.focusedWindow():moveToUnit(rect(size1, 0, size1, 1))
+  end
 
-function cmd.snapWindowLeftHalf()
-  hs.window.focusedWindow():moveToUnit(hs.layout.left50)
-end
+  function cmd.snapWindowLeftBig()
+    hs.window.focusedWindow():moveToUnit(rect(0, 0, size2, 1))
+  end
 
-function cmd.snapWindowRightHalf()
-  hs.window.focusedWindow():moveToUnit(hs.layout.right50)
-end
+  function cmd.snapWindowLeftSmall()
+    hs.window.focusedWindow():moveToUnit(rect(0, 0, size1, 1))
+  end
 
-function cmd.snapWindowRightBig()
-  hs.window.focusedWindow():moveToUnit(g.right_big_rect)
-end
+  function cmd.snapWindowLeftHalf()
+    hs.window.focusedWindow():moveToUnit(rect(0, 0, 1/2, 1))
+  end
 
-function cmd.snapWindowRightSmall()
-  hs.window.focusedWindow():moveToUnit(g.right_small_rect)
-end
+  function cmd.snapWindowRightHalf()
+    hs.window.focusedWindow():moveToUnit(rect(1/2, 0, 1/2, 1))
+  end
 
-function cmd.maximizeWindow()
-  hs.window.focusedWindow():moveToUnit(hs.layout.maximized)
+  function cmd.snapWindowRightBig()
+    hs.window.focusedWindow():moveToUnit(rect(1 - size2, 0, size2, 1))
+  end
+
+  function cmd.snapWindowRightSmall()
+    hs.window.focusedWindow():moveToUnit(rect(1 - size1, 0, size1, 1))
+  end
+
+  function cmd.maximizeWindow()
+    hs.window.focusedWindow():moveToUnit(rect(0, 0, 1, 1))
+  end
 end
 
 function cmd.maximizeAllWindows()
@@ -89,10 +142,6 @@ end
 
 function cmd.showApplicationName()
   hs.alert.show(hs.window.focusedWindow():application():name())
-end
-
-function cmd.reload()
-  hs.reload()
 end
 
 function cmd.toggleConsole()
@@ -111,49 +160,4 @@ function cmd.typeUnixTime()
   hs.eventtap.keyStrokes(tostring(os.time()))
 end
 
-function hex(code, alpha)
-  alpha = alpha or 100
-  return {
-    hex = "#" .. code,
-    alpha = alpha / 100
-  }
-end
-
--- Configuration
-hs.window.animationDuration = 0
-
-hs.alert.defaultStyle.strokeWidth = 8
-hs.alert.defaultStyle.strokeColor = hex"222222"
-hs.alert.defaultStyle.fillColor = hex"ffffff"
-hs.alert.defaultStyle.textColor = hex"222222"
--- hs.alert.defaultStyle.textFont = ".AppleSystemUIFont"
-hs.alert.defaultStyle.textSize = 32
-hs.alert.defaultStyle.radius = 8
--- hs.alert.defaultStyle.atScreenEdge = 0
-hs.alert.defaultStyle.fadeInDuration = 0
-hs.alert.defaultStyle.fadeOutDuration = 0
-hs.alert.defaultStyle.padding = 16
-
-prefix = {"ctrl", "alt"}
-
-hs.hotkey.bind(prefix, "y", cmd.unhideAll)
-hs.hotkey.bind(prefix, "h", cmd.hidePersonalApps)
-
-hs.hotkey.bind(prefix, "m", cmd.snapWindowLeftSmall)
-hs.hotkey.bind(prefix, "u", cmd.snapWindowLeftBig)
-hs.hotkey.bind(prefix, "j", cmd.snapWindowLeftHalf)
-hs.hotkey.bind(prefix, "k", cmd.maximizeWindow)
-hs.hotkey.bind(prefix, "l", cmd.snapWindowRightHalf)
-hs.hotkey.bind(prefix, "o", cmd.snapWindowRightBig)
-hs.hotkey.bind(prefix, ".", cmd.snapWindowRightSmall)
-
-hs.hotkey.bind(prefix, "d", cmd.typeDate)
-hs.hotkey.bind(prefix, "t", cmd.typeTime)
-hs.hotkey.bind(prefix, "x", cmd.typeUnixTime)
-
-hs.hotkey.bind(prefix, "space", cmd.maximizeAllWindows)
-hs.hotkey.bind(prefix, "v", cmd.showApplicationName)
-hs.hotkey.bind(prefix, "r", cmd.reload)
-hs.hotkey.bind(prefix, "c", cmd.toggleConsole)
-
-hs.alert.show("Hammerspoon loaded", 0.5)
+main()
